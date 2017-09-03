05a70b62e043c00636f11a739a49c86e53619524
Jorge <jcvarela@live.com>
2017-09-03 18:32:36 -0400

    finish assignment 2

--- Assignment 2/listingController.js
+++ Assignment 2/listingController.js
@@ -1,21 +1,49 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = {};
     var INFO = "Detailed Information";
     $scope.detailedInfo.title = INFO;
+    var itemsToRemove = [];
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function(newFactory){
-        $scope.listings.push(newFactory);
+    $scope.addListing = function(){
+        $scope.listings.unshift($scope.newItem);
     };
 
-    $scope.deleteListing = function(index) {
-      $scope.listings.splice(index,1);
+     var isButtonDisable = function(check){
+         if(check){
+             itemsToRemove = [];
+             $("#deleteButton").prop("disabled", true);
+             $("#cancelButton").prop("disabled", true);
+         }else {
+             $("#deleteButton").prop("disabled", false);
+             $("#cancelButton").prop("disabled", false);
+         }
+     };
+
+    $scope.canceldeleteListing = function(){
+        for(var i =0; i < $scope.listings.length; i++) {
+            if($scope.listings[i].check){
+                $scope.listings[i].check = false;
+            }
+        }
+        isButtonDisable(true);
     };
+
+    $scope.deleteListing = function() {
+        for(var i =0; i < $scope.listings.length; i++) {
+           if($scope.listings[i].check){
+               $scope.listings.splice(i,1);
+               i--;
+           }
+        }
+        isButtonDisable(true);
+    };
+
     $scope.showDetails = function(index) {
         $scope.detailedInfo = {};
       if(index === -1 || index >= $scope.listings.length){
@@ -34,6 +62,22 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
         }
       }
     };
-  }
+    $scope.openNewItemModel = function(){
+          $scope.newItem = {};
+      };
+      $scope.checkChange = function(index){
+          if(index < 0 || index > $scope.listings.length)
+              return;
+
+          if(itemsToRemove[index] === 1){
+              itemsToRemove.splice(index, 1);
+          }
+          else {
+              itemsToRemove[index] = 1;
+          }
+
+          isButtonDisable(!(itemsToRemove.length > 0));
+      };
+}
 ]);
 
