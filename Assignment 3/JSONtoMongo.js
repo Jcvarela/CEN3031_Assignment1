b55938290f5428fa8d358a71380ff9c71ef34bfa
Jorge <Jcvarela@live.com>
2017-09-06 17:11:45 -0400

    almost done with assignment3

--- Assignment 3/JSONtoMongo.js
+++ Assignment 3/JSONtoMongo.js
@@ -7,7 +7,19 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config'),
+    listingData = [];
+
+
+mongoose.connect(config.db.uri);
+
+var connection = mongoose.connection;
+
+connection.on("connected", function() {
+    console.log("Connected!!!!");
+    setData();
+});
+
 
 /* Connect to your database */
 
@@ -16,8 +28,43 @@ var fs = require('fs'),
   and then save it to your Mongo database 
  */
 
-
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
  */
+
+
+var setData = function() {
+    return new Promise(function (resolve, reject) {
+        fs.readFile('listings.json', 'utf8', function (err, data) {
+            if (err) {
+                return reject(err);
+            }
+            
+            return resolve(JSON.parse(data));
+        })
+    }).then(function (input) {
+
+        return new Promise(function (resolve, reject){
+            Listing.remove({}, function(err){
+                if(err) return reject( err);
+                console.log("remove DB");
+
+                return resolve();
+            });
+        }).then(function(){
+            for (var obj in input["entries"]) {
+                var entry = new Listing(input["entries"][obj]);
+
+                //saving to the database
+                entry.save(function (err) {
+                    if (err) {
+                        console.log("Fail to save User " + err)
+                    } else {
+                        console.log("User save successfully");
+                    }
+                });
+            }
+        });
+    });
+};
--- Assignment 3/queries.js
+++ Assignment 3/queries.js
@@ -1,10 +1,32 @@
 /* Fill out these functions using Mongoose queries*/
+var mongoose = require('mongoose'),
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
+
+
+// mongoose.connect(config.db.uri);
+//
+// var connection = mongoose.connection;
+//
+// connection.on("connected", function() {
+//     // findLibraryWest();
+//     // removeCable();
+//     // updatePhelpsLab();
+//     retrieveAllListings();
+//     // console.log("connected!!!!");
+// });
+
+
 
 var findLibraryWest = function() {
   /* 
     Find the document that contains data corresponding to Library West,
     then log it to the console. 
    */
+  Listing.find({name:"Library West"}, function (err, obj){
+      if(err) throw err;
+      console.log(obj);
+  });
 };
 var removeCable = function() {
   /*
@@ -12,17 +34,34 @@ var removeCable = function() {
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
     and remove this listing from your database and log the document to the console. 
    */
+    Listing.remove({code:"CABL"}, function (err){
+        if(err) throw err;
+        console.log("remove Cable");
+    });
 };
 var updatePhelpsLab = function() {
   /*
     Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
     log the updated document to the console. 
    */
+    Listing.findOneAndUpdate({name :"Phelps Laboratory"},{ address: '102 Phelps Lab, Gainesville, FL 32611' }, function (err, obj){
+        if(err) throw err;
+        console.log(obj);
+
+        Listing.find({name :"Phelps Laboratory"},function (err, obj) {
+            if(err) throw err;
+            console.log(obj);
+        });
+    });
 };
 var retrieveAllListings = function() {
   /* 
     Retrieve all listings in the database, and log them to the console. 
    */
+    Listing.find({}, function (err, obj){
+        if(err) throw err;
+        console.log(obj);
+    });
 };
 
 findLibraryWest();
