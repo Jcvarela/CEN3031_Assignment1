fe906165a65f05cc39d6d9d93c8841edabe559a9
Jorge <Jcvarela@live.com>
2017-09-22 12:53:08 -0400

    change port to PORT

--- server/config/app.js
+++ server/config/app.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose'),
     express = require('./express');
 
 module.exports.start = function() {
-  var port = ( process.env.port || 8080);
+  var port = ( process.env.PORT || 8080);
   var app = express.init();
   app.listen(port, function() {
     console.log('App listening on port', port);
